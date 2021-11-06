4b660eb5d7c9259aa34215eac03a7fa885f6ce6d
eacmesquita <31151423+eacmesquita@users.noreply.github.com>
2021-11-06 22:47:18 +0530

    Upload metadata with file to cloud

--- tools/cloud/gcp_upload.py
+++ tools/cloud/gcp_upload.py
@@ -1,8 +1,8 @@
-
 import sys
 from google.cloud import storage
+import json
 
-def upload_blob(bucket_name, key_file, source_file_name, destination_blob_name):
+def upload_blob(bucket_name, key_file, source_file_name, destination_blob_name, blob_metadata):
     """Uploads a file to the bucket."""
 
     storage_client = storage.Client.from_service_account_json(json_credentials_path=key_file)
@@ -14,6 +14,10 @@ def upload_blob(bucket_name, key_file, source_file_name, destination_blob_name):
     print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
     print("Public url: https://{}.storage.googleapis.com/{}".format(bucket_name, destination_blob_name))
 
+    blob = bucket.get_blob(destination_blob_name)
+    blob.metadata = blob_metadata
+    blob.patch()
+    print("The metadata for the blob {} is {}".format(blob.name, blob.metadata))
 
 if __name__ == "__main__":
     upload_blob(
@@ -21,4 +25,5 @@ if __name__ == "__main__":
         key_file = sys.argv[2],
         source_file_name=sys.argv[3],
         destination_blob_name=sys.argv[4],
+        blob_metadata=json.loads(sys.argv[5])
     )
